df5a29df67afed9028a07711ca907bda8563da96
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2024-09-04 15:44:08 -0300

    Clairty

--- screenli-cli/src/app/layout.tsx
+++ screenli-cli/src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Screenli | Desafio Verzel",
@@ -43,6 +44,17 @@ export default function RootLayout({
           content="Faça sua lista de filmes."
         />
         <meta property="twitter:image" content="/cover.png" />
+
+        {/* Microsoft Clairity */}
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ny1ia4yk10"); 
+           `}
+        </Script>
       </head>
       <body className={inter.className}>
         {children}
